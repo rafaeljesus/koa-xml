e03e13771bf02422e14f089b24d907e1f476b322
rafaeljesus <rafaelljesus86@gmail.com>
2015-10-04 12:21:15 -0300

    Cover specs deeper
    Now spec are covered as integration tests, creating a koa instance, listen to server and send a real xml request
    
    Fow now, the only method covered is POST, next metho will be covered on next commit

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,37 +1,43 @@
 'use strict'
 
-const mocha = require('mocha')
-  , coMocha = require('co-mocha')
-  , chai    = require('chai')
-  , expect  = chai.expect
-  , koaXml  = require('../')
+const mocha       = require('mocha')
+  , coMocha       = require('co-mocha')
+  , chai          = require('chai')
+  , koa           = require('koa')
+  , request       = require('supertest')
+  , expect        = chai.expect
+  , koaXml        = require('../')
 
 coMocha(mocha)
 
 describe('IndexSpec', () => {
 
   const options = {}
-  const methods = ['POST', 'PUT', 'PATCH', 'TRACE']
   const next = function* () {}
 
   describe('.skip', () => {
 
+    let koa = Object.create(null)
+
+    beforeEach(() => {
+      koa = {is: () => false, request: {}}
+    })
+
     it('should skip if content type is not xml', function* () {
-      const koa = {is: () => false, request: {}}
       const fn = koaXml(options)
       yield fn.call(koa, next)
       expect(koa.request.body).to.not.exist
     })
 
     it('should skip if method is GET', function* () {
-      const koa = {is: () => true, request: {}, method: 'GET'}
+      koa.method = 'GET'
       const fn = koaXml(options)
       yield fn.call(koa, next)
       expect(koa.request.body).to.not.exist
     })
 
     it('should skip if method is DELETE', function* () {
-      const koa = {is: () => true, request: {}, method: 'DELETE'}
+      koa.method = 'DELETE'
       const fn = koaXml(options)
       yield fn.call(koa, next)
       expect(koa.request.body).to.not.exist
@@ -39,21 +45,25 @@ describe('IndexSpec', () => {
 
   })
 
-  methods.map(m => {
+  describe('.POST', () => {
 
-    describe.skip('.' + m, () => {
+    it('should parse xml to json', done => {
+      const app = koa()
 
-      const koa = {
-          is: () => true
-        , request: {}
-        , method: 'POST'
-      }
-
-      it('should parse xml to json', function* () {
-        const fn = koaXml(options)
-        yield fn.call(koa, next)
-        expect(koa.request.body).to.not.exist
+      app.use(koaXml())
+      app.use(function* () {
+        expect(this.request.body).to.be.eql({name: 'foo'})
+        this.status = 200
       })
+
+      const server = app.listen()
+        , xml = '<name>foo</name>'
+
+      request(server).
+        post('/').
+        type('xml').
+        send(xml).
+        expect(200, done)
     })
   })
 
